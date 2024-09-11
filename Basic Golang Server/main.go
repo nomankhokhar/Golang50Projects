b964020f6f107d5684acb500dd0bb9a5b105dcd8
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-09-11 19:09:39 +0500

    Build Basic Golang Server and Serve the API's

---
+++ Basic Golang Server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./src"))
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/hello", helloFunc)
+	http.HandleFunc("/form", formFunc)
+
+	fmt.Printf("Server is running on port 8080\n")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func formFunc(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+
+	name := r.FormValue("name");
+	address:= r.FormValue("address");
+
+	fmt.Fprintf(w, "Name : %s\n", name)
+	fmt.Fprintf(w, "Address : %s\n" , address)	
+}
+
+
+func helloFunc(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404  not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
