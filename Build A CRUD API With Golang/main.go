798ef738ece4a73a61c5820522768f92595bdc49
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-09-12 11:53:17 +0500

    func: Define Routes

--- Build A CRUD API With Golang/main.go
+++ Build A CRUD API With Golang/main.go
@@ -2,21 +2,58 @@ package main
 
 import (
 	"fmt"
-	// "log"
-	// "encoding/json"
-	// "math/rand"
-	// "net/http"
-	// "github.com/gorilla/mux"
-	// "strconv"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type Movie struct{
-	ID string "json:id"
-	Isbn string "json:isbn"
-	Title string "json:title"
+type Movie struct {
+	ID       string    "json:id"
+	Isbn     string    "json:isbn"
+	Title    string    "json:title"
 	Director *Director "json:director"
 }
 
+type Director struct {
+	FirstName string "json:firstname"
+	LastName  string "json:lastname"
+}
+
+var movies []Movie
+
 func main() {
-	fmt.Println("Hello World")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movie/{id}", getMovie).Methods("GET")
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Server is running on port 8000")
+	http.ListenAndServe(":8000", r)
+}
+
+func getMovie(){
+	fmt.Println("Get Movie")
+}
+
+func getMovies(){
+	fmt.Println("Get Movies")
+}
+
+func createMovie(){
+	fmt.Println("Create Movie")
+}
+
+func updateMovie(){
+	fmt.Println("Update Movie")
+}
+
+func deleteMovie(){
+	fmt.Println("Delete Movie")
 }
