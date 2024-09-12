0d654e57fa186d8b19090882d960369bb7b82630
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-09-12 09:59:15 +0500

    Build CRUD Movie DB

--- Build A CRUD API With Golang/main.go
+++ Build A CRUD API With Golang/main.go
@@ -1,6 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "log"
+	// "encoding/json"
+	// "math/rand"
+	// "net/http"
+	// "github.com/gorilla/mux"
+	// "strconv"
+)
+
+type Movie struct{
+	ID string "json:id"
+	Isbn string "json:isbn"
+	Title string "json:title"
+	Director *Director "json:director"
+}
 
 func main() {
 	fmt.Println("Hello World")
