9dbc52e6ee8e5331d45b783d8ca89dcfe9768fb4
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-09-18 16:46:51 +0500

    Create, Delete, and GetUser Struct Method

--- Basic Golang API with MongoDB/controller/userController.go
+++ Basic Golang API with MongoDB/controller/userController.go
@@ -1,1 +1,89 @@
-package controller
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"golangMongoDB/models"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// UserController struct
+
+type UserController struct{
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController {
+	return &UserController{s}
+}
+
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	u := models.User{}
+
+	if err := uc.session.DB("go-web-dev-db").C("users").FindId(oid).One(&u); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	uj, err := json.Marshal(u)
+
+	if err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s", uj)
+}
+
+func (uc UserController) CreateUser (w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := models.User{}
+
+	json.NewDecoder(r.Body).Decode(&u) // Decode the request body into the struct
+
+	u.Id = bson.NewObjectId()
+	uc.session.DB("go-web-dev-db").C("users").Insert(u)
+
+	uj, err := json.Marshal(u) // Marshal the struct into a json object
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s", uj)
+}
+
+func (uc UserController) DeleteUser (w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		w.WriteHeader(404)
+		return
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	if err := uc.session.DB("go-web-dev-db").C("users").RemoveId(oid); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Deleted user", oid, "\n")	
+}
--- Basic Golang API with MongoDB/main.go
+++ Basic Golang API with MongoDB/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"golangMongoDB/controllers"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r:= httprouter.New()
+
+	uc := controllers.NewUserController(getSession())
+
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+
+	http.ListenAndServe("localhost:8080", r)
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
